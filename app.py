c7106d751f653092f755a1646c2e630d490594cd
Alexander Crabbe <alienzen@hotmail.com>
2024-10-04 12:53:12 +0000

    add streamlit ml performance page

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ import streamlit as st
 from app_pages.multipage import MultiPage
 
 from app_pages.page_summary import page_summary_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
 
 
 app = MultiPage(app_name="Brain Tumour Classifier")
 
 app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("ML performance Metrics", page_ml_performance_metrics)
 
 app.run()
